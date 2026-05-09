0688e4bb3279b0939d1b5b12228c22e93633035f
shreeramcomputers <shreeramcomputers45@gmail.com>
2026-05-10 00:56:06 +0530

    Update SRCStringHelper.java

--- SRCStringHelper/src/com/src/stringhelper/SRCStringHelper.java
+++ SRCStringHelper/src/com/src/stringhelper/SRCStringHelper.java
@@ -1,19 +1,33 @@
 package com.src.stringhelper;
 
+import com.google.appinventor.components.annotations.DesignerComponent;
 import com.google.appinventor.components.annotations.SimpleFunction;
 import com.google.appinventor.components.annotations.SimpleObject;
+
+import com.google.appinventor.components.common.ComponentCategory;
+
 import com.google.appinventor.components.runtime.AndroidNonvisibleComponent;
 import com.google.appinventor.components.runtime.ComponentContainer;
 
+@DesignerComponent(
+        version = 1,
+        description = "String Helper Extension by SRC",
+        category = ComponentCategory.EXTENSION,
+        nonVisible = true,
+        iconName = "assets/icon.png"
+)
+
 @SimpleObject(external = true)
+
 public class SRCStringHelper extends AndroidNonvisibleComponent {
 
     public SRCStringHelper(ComponentContainer container) {
         super(container.$form());
     }
 
-    @SimpleFunction(description = "Text mein total words count karke return karta hai.")
+    @SimpleFunction(description = "Count total words")
     public int CountWords(String text) {
+
         if (text == null || text.trim().isEmpty()) {
             return 0;
         }
@@ -21,8 +35,9 @@ public class SRCStringHelper extends AndroidNonvisibleComponent {
         return text.trim().split("\\s+").length;
     }
 
-    @SimpleFunction(description = "Text ko reverse karke return karta hai.")
+    @SimpleFunction(description = "Reverse text")
     public String ReverseText(String text) {
+
         if (text == null) {
             return "";
         }
@@ -30,7 +45,7 @@ public class SRCStringHelper extends AndroidNonvisibleComponent {
         return new StringBuilder(text).reverse().toString();
     }
 
-    @SimpleFunction(description = "Check karta hai ki text palindrome hai ya nahi.")
+    @SimpleFunction(description = "Check palindrome")
     public boolean IsPalindrome(String text) {
 
         if (text == null || text.isEmpty()) {
